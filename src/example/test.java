7356e20a2a3e3a729296cba3580a02777b927cce
tyler <tyler@tyler-pc>
2018-11-09 05:22:47 -0500

    added main class

---
+++ src/example/test.java
@@ -0,0 +1,13 @@
+package example;
+
+public class test {
+	
+	public static void main(String[] args) {
+		System.out.println("Hello Git");
+	}
+	
+	public static void yell() {
+		System.out.println();
+	}
+
+}
